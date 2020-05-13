27ce353b8ff255c315054f32b37759b46b0c5723
DonRoden <gazalad-r.o.b@hotmail.fr>
2020-05-13 18:14:07 +0200

    Removed bootstrap components from home. Added fetch nextEvent on homepage

--- src/components/common/modals/EventModal.jsx
+++ src/components/common/modals/EventModal.jsx
@@ -6,6 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Box from '@material-ui/core/Container';
+
+import { display } from '@material-ui/system';
 
 
 class EventModal extends React.Component {
@@ -22,7 +25,7 @@ class EventModal extends React.Component {
     }
 
     subscribe() {
-        
+
     }
 
     onChange(e) {
@@ -31,23 +34,23 @@ class EventModal extends React.Component {
 
     render() {
         return (
-            <div>
+            <>
                 <Button size="sm" id="subscribe" onClick={() => this.setModalShow(true)}>
                     {this.props.children}
                 </Button>
 
                 <Modal show={this.state.modalShow}
                 onHide={() => this.setModalShow(false)}
-                size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+                size="lg" centered>
                     <Modal.Header closeButton>
                         <Modal.Title id="contained-modal-title-vcenter">
                           {this.props.event.title}
                         </Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <h4>Venez nombreux à {this.props.event.adress} le {this.props.event.date}</h4>
+                        <h4>Venez nombreux au {this.props.event.address} le {this.props.event.startDateTime}</h4>
                         <p>
-                          {this.props.event.desc}
+                          {this.props.event.description}
                         </p>
                     </Modal.Body>
                     <Modal.Footer>
@@ -57,7 +60,7 @@ class EventModal extends React.Component {
                         </Form>
                     </Modal.Footer>
                 </Modal>
-            </div>
+            </>
         );
     }
 }
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,28 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import EventModal from '../../common/modals/EventModal'
+import ApiService from "../../../services/ApiService";
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid'
+import { display } from '@material-ui/system';
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            nextEvent: {
+                title:'',
+                address:'',
+                description:'',
+                startDateTime:''
+            }
+        }
+        this.getNextEvent = this.getNextEvent.bind(this);
+    }
+
+    componentDidMount() {
+        this.getNextEvent();
+    }
+
+    getNextEvent() {
+        let data;
+        ApiService.fetchEvents()
+            .then(res => {
+                if (res.data.length > 0) {
+                    let datesArray = res.data.map(event => event.startDateTime);
+                    let nextEvent = res.data[this.minDateIndex(datesArray)];
+                    nextEvent.startDateTime = new Date(nextEvent.startDateTime)
+                        .toLocaleString("fr-FR", {day: "numeric", month:"long", year: "numeric", hour: "numeric", minute: "numeric"});
+                    this.setState({nextEvent: nextEvent});
+                }
+            });
+    }
+
+    minDateIndex(dates) {
+        let minIndex = 0;
+        dates.map(date => {
+            if (date < dates[minIndex]) minIndex = date.index;
+        });
+        return minIndex;
+    }
+
     render() {
         return (
-            <Container id="content" fluid>
-                <Row className="box">
-                    <p>Prochaine marche le {this.props.event}</p>
-                    <EventModal event={{title: "Ramassage des déchets", adress: "Issy", date: "11 mai", description: "Venez ramassez les poubelles ! Sah quel plaisir !"}}>
+            <Container maxWidth="lg">
+                <Box>
+                    <Typography variant="h6" display="inline"> Prochaine marche le {this.state.nextEvent.startDateTime}</Typography>
+                    <EventModal event={this.state.nextEvent}>
                         Participer
                     </EventModal>
-                </Row>
-                <Row className="box" id="site-pitch">
-                    <p>Grâce à Clean2Gather, participe au nettoyage de ta commune.<br/>
-                    Signale les zones à déchets ou participe à une marche de nettoyage collaborative pour rendre ta ville plus propre !</p>
-                </Row>
-                <Row className="box">
-                    <p>{this.props.events} marches effectuées et plus de {this.props.markers} zones à déchets signalées</p>
-                </Row>
+                </Box>
+                <Box>
+                    <Typography variant="h6">Grâce à Clean2Gather, participe au nettoyage de ta commune.<br/>
+                    Signale les zones à déchets ou participe à une marche de nettoyage collaborative pour rendre ta ville plus propre !</Typography>
+                </Box>
+                <Box>
+                    <Typography variant="h6">{this.props.events} marches effectuées et plus de {this.props.markers} zones à déchets signalées</Typography>
+                </Box>
             </Container>
         );
     }

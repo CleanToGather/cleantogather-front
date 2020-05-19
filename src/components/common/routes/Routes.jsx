dd6818c76912ab9b7f728bfc6bc26758a0637b3d
Pombet <44603016+Pombet@users.noreply.github.com>
2020-05-19 15:11:57 +0200

    Reset routes

--- src/components/common/routes/Routes.jsx
+++ src/components/common/routes/Routes.jsx
@@ -1,10 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import ListUser from "../../pages/user/ListUser";
-import ListMarker from "../../pages/signalements/ListMarkers";
 import AddUser from "../../pages/user/AddUser";
 import EditUser from "../../pages/user/EditUser";
-import CalendarEvent from "../../pages/event/CalendarEvent";
-import Formulaire from "../../pages/signalements/Formulaire";
 import React from "react";
 
 const AppRouter = () => {
@@ -14,12 +11,8 @@ const AppRouter = () => {
 		<Switch>
 		    <Route path="/" exact component={ListUser} />
 		    <Route path="/users" component={ListUser} />
-		    <Route path="/" exact component={ListMarker} />
-		    <Route path="/markers" component={ListMarker} />
 		    <Route path="/add-user" component={AddUser} />
 		    <Route path="/edit-user" component={EditUser} />
-		    <Route path="/event/calendar" component={CalendarEvent} />
-		    <Route path="/signalements/formulaire" component={Formulaire} />
 		</Switch>
 	    </Router>
 	</div>
